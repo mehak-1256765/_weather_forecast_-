3385971fcff7093342d884ddc091b6e0abb3a235
Mehak Thakue <mehakmanhas19@gmail.com>
2023-12-30 21:59:51 +0530

    third commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "0551a97e3eac66c73183e28750c635cd",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
